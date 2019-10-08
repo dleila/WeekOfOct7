fb056d8c0263a3887715c5617b6a840b5105498e
Dahbi <54712900+dleila@users.noreply.github.com>
2019-10-08 08:49:34 -0500

    Add files via upload
    Fourth version of my work on 10-8-19.

--- NewWork.py
+++ NewWork.py
@@ -26,11 +26,14 @@ print(w + e)
 
 # More stuff 10-8
 
+# Saying Mary had a little lamb
 print("Mary had a little lamb.")
 print("Its fleece was white as %s." % 'snow')
 print("And everywhere that Mary went.")
+# Had the "." multiplied by 10 making it become ".........."
 print("." * 10)
 
+# Making definitions for end1-12
 end1 = "C"
 end2 = "h"
 end3 = "e"
@@ -44,13 +47,19 @@ end10 = "g"
 end11 = "e"
 end12 = "r"
 
+# Stringing the word together by adding it
 print(end1 + end2 + end3 + end4 + end5 + end6 + end7 + end8 + end9 + end10 + end11 + end12)
 
 # More Formatting
 formatter = "%r %r %r %r"
+# Numbers in actual number form
 print(formatter % (1, 2, 3, 4))
+# Numbers in letter form
 print(formatter % ("one", "two", "three", "four"))
+# Using formatter to put in what I put in the second parentheses without commas
 print(formatter % (True, False, False, True))
+# Formatter in %r as I said up above so when I format formatter I get a lot of %rs
 print(formatter % (formatter, formatter, formatter, formatter))
 
 # Why did I use %r instead of %s?
+# Because %r can be put there all by itself while %s needs a sentence to be places in
